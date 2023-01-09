3a37e5fd173aa14dd38c90ac3b9316f21d05470c
heloisa <45131010+annaheloiss@users.noreply.github.com>
2023-01-08 22:04:52 -0300

    Update routes.js

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -8,35 +8,33 @@ app.use(express.json());
 app.get('/', (req, res) => res.send("Homepage! | Pagina Inicial!"));
 
 
-// Request for creating Book | Requisição para cadastrar livro.
+// Request for creating the Book | Requisição para cadastrar livro
 app.post('/books/insert', async (req, res) => {
     await insertBook(req.body)
     res.status(200).send("O livro foi cadatrado com sucesso!");
 })
 
-// Requisição para procurar livro.
+// Request to search book | Requisição para procurar livro
 app.get('/books/find', async (req, res) => {
     let books = await findBook()
     res.status(200).json(books)
 })
 
- //Requisição para procurar livro através do id
+// Request to search for book by id | Requisição para procurar livro através do id
     app.get('/books/:id', async (req, res) => {
     let books = await findBook(req.params.id)    
     res.status(200).json(books);
 
 })
 
-
-
-// Request to update Book | Requisição para atualizar o livro.
+// Request to update the Book | Requisição para atualizar o livro
 app.put('/books/update/:id', async (req, res) => {
     await updateBook(req.params.id, req.body)
     res.status(200).send("Livro atualizado com sucesso!")
 })
 
 
-//
+// Request to delete the Book | Requisição para deletar o livro
 app.delete('/books/delete/:id', async(req,res) =>{
     await deleteBook (req.params.id)
     res.status(200).send("O livro foi excluido com sucesso!")
