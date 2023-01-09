26d8ed2cdd95b1ab24ffe45207ef6ee62af7a3bd
heloisa <45131010+annaheloiss@users.noreply.github.com>
2023-01-08 21:37:23 -0300

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-import { startDB } from './src/db/database.js';
-import app from './src/routes/routes.js';
-startDB();
-const port = 3000;
-
-app.listen(port, () => console.log(`Apps running on port ${port}`));
